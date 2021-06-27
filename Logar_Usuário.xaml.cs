c933c5ec5f9dfa1ee716353ecd8a105769d28b93
Maria Eduarda Braz <mariaeduardbraz413@gmail.com>
2021-06-27 19:16:48 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ Logar_Usuário.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace projeto_sgp_WPFversion
+{
+    /// <summary>
+    /// Lógica interna para Logar_Usuário.xaml
+    /// </summary>
+    public partial class Logar_Usuário : Window
+    {
+        public Logar_Usuário()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
