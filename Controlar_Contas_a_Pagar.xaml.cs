23503e4e3fe60b0a0fa436b66ed65eaf107f1384
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2021-06-29 20:59:20 -0300

    Reset

--- Controlar_Contas_a_Pagar.xaml.cs
+++ Controlar_Contas_a_Pagar.xaml.cs
@@ -23,5 +23,40 @@ namespace projeto_sgp_WPFversion
         {
             InitializeComponent();
         }
+
+        private void tela_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnMax_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void btnMin_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            Tela_cancel tela_Cancel = new Tela_cancel();
+            tela_Cancel.ShowDialog();
+
+        }
+
     }
 }
