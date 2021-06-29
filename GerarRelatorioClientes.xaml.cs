e284a6c8bdb9de27621ff3f357afc995fb8833fb
Ma7urana <gabriel.maturana.te@gmail.com>
2021-06-29 02:34:05 -0400

    desenvolver tela GerarRelatorioClientes

---
+++ GerarRelatorioClientes.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace projeto_sgp_WPFversion
+{
+    /// <summary>
+    /// Interaction logic for GerarRelatorioClientes.xaml
+    /// </summary>
+    public partial class GerarRelatorioClientes : Window
+    {
+        public GerarRelatorioClientes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void btnMax_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+        private void btnMin_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void tela_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void back(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void destaques(object sender, RoutedEventArgs e)
+        {
+            GerarRelatorioClientesDestaques rdestaques = new GerarRelatorioClientesDestaques();
+            rdestaques.ShowDialog();
+        }
+
+        private void situacao(object sender, RoutedEventArgs e)
+        {
+            GerarRelatorioSituacaoClientes rsituacao = new GerarRelatorioSituacaoClientes();
+            rsituacao.ShowDialog();
+        }
+    }
+}
