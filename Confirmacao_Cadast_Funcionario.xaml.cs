c6e9d00b9a6babc4521362f383f418bd22c9e2a1
Natália <heitorfernandes812@gmail.com>
2021-06-30 16:18:50 -0400

    Adicionando botão na tela de confirmação de Cadastro de Funcionário

--- Confirmacao_Cadast_Funcionario.xaml.cs
+++ Confirmacao_Cadast_Funcionario.xaml.cs
@@ -23,5 +23,10 @@ namespace projeto_sgp_WPFversion
         {
             InitializeComponent();
         }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
