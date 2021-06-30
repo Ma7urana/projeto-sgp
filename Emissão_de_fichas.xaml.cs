6f5eb85cbbeb6f0f054ac86fcd15b600b249a7e0
Maria Eduarda Braz <mariaeduardbraz413@gmail.com>
2021-06-29 23:18:06 -0400

    Criando emissão de fichas

---
+++ Emissão_de_fichas.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace projeto_sgp_WPFversion
+{
+    /// <summary>
+    /// Lógica interna para Emissão_de_fichas.xaml
+    /// </summary>
+    public partial class Emissão_de_fichas : Window
+    {
+        public Emissão_de_fichas()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVoltar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
