8ddc8185f63085b362278239f5c882d899c948e6
Natália <heitorfernandes812@gmail.com>
2021-06-30 16:06:56 -0400

    Criando botões na tela Controle de Compras

--- Controlar_Compras.xaml.cs
+++ Controlar_Compras.xaml.cs
@@ -23,5 +23,28 @@ namespace projeto_sgp_WPFversion
         {
             InitializeComponent();
         }
+
+        private void btnMin_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnMax_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
