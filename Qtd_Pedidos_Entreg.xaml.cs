55f7dbcd47ae4dbbc9223845cf1abf42b9d15bb7
Natália <heitorfernandes812@gmail.com>
2021-06-30 17:23:23 -0400

    Adicionando botões na tela Quantidade de Produtos Entregue

--- Qtd_Pedidos_Entreg.xaml.cs
+++ Qtd_Pedidos_Entreg.xaml.cs
@@ -23,5 +23,37 @@ namespace projeto_sgp_WPFversion
         {
             InitializeComponent();
         }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnMax_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void btnMin_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Cons_Qtd_Ped_Entrg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
