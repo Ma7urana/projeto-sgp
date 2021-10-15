913c5ac1cc510e73e3a092179911aa009983978c
maturanei <maturana.te@protonmail.com>
2021-10-14 18:11:06 -0700

    adicionar Read de Vendas

--- Models/Venda.cs
+++ Models/Venda.cs
@@ -8,7 +8,20 @@ namespace projeto_sgp_WPFversion.Models
 {
     class Venda
     {
+        public int Id { get; set; }
+        
+        public DateTime Data { get; set; }
 
+        public float Subtotal { get; set; }
 
+        public float Desconto { get; set; }
+
+        public float Troco { get; set; }
+
+        public float ValorTotal { get; set; }
+
+        public string FormaPagamento { get; set; }
+
+        public int Parcelas { get; set; } 
     }
 }
--- Models/VendaDAO.cs
+++ Models/VendaDAO.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using projeto_sgp_WPFversion.Interface;
+using projeto_sgp_WPFversion.DataBase;
+using MySql.Data.MySqlClient;
 
 namespace projeto_sgp_WPFversion.Models
 {
     class VendaDAO : IDAO<Venda>
     {
+        private static Conexao conn;
+
+        public VendaDAO()
+        {
+            conn = new Conexao();
+        }
+
         public void Delete(Venda t)
         {
             throw new NotImplementedException();
@@ -26,7 +35,40 @@ namespace projeto_sgp_WPFversion.Models
 
         public List<Venda> List()
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<Venda> vendas = new List<Venda>();
+
+                var query = conn.Query();
+                query.CommandText = "SELECT * FROM venda";
+
+                MySqlDataReader reader = query.ExecuteReader();
+
+                while(reader.Read())
+                {
+                    vendas.Add(new Venda()
+                    {
+                        Id = reader.GetInt32("Id_vend"),
+                        Data = reader.GetDateTime("Data_Vend"),
+                        Subtotal = reader.GetFloat("Subtotal_Vend"),
+                        Desconto = reader.GetFloat("Desconto_Vend"),
+                        Troco = reader.GetFloat("Troco_Vend"),
+                        ValorTotal = reader.GetFloat("Valor_Total_Vend"),
+                        FormaPagamento = reader.GetString("FormaPag_Vend"),
+                        Parcelas = reader.GetInt32("Qtd_parcelas_Vend")
+                    });
+                }
+
+                return vendas;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.close();
+            }
         }
 
         public void Update(Venda t)
