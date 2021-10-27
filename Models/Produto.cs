23f4f87b35f34d0325356db7eb9ac1ae07617892
maturanei <maturana.te@gmail.com>
2021-10-27 19:10:24 -0400

    implementar insert e read

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -8,5 +8,11 @@ namespace projeto_sgp_WPFversion.Models
 {
     class Produto
     {
+        public uint Id { get; set; }
+        public string Nome { get; set; }
+        public float PrecoCompra { get; set; }
+        public float PrecoVenda { get; set; }
+        public uint Quantidade { get; set; }
+        public uint FornecedorId { get; set; }
     }
 }
--- Models/ProdutoDAO.cs
+++ Models/ProdutoDAO.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using projeto_sgp_WPFversion.Interface;
+using projeto_sgp_WPFversion.DataBase;
+using MySql.Data.MySqlClient;
 
 namespace projeto_sgp_WPFversion.Models
 {
     class ProdutoDAO : IDAO<Produto>
     {
+        private readonly Conexao con;
+
+        public ProdutoDAO()
+        {
+            con = new Conexao();
+        }
+
         public void Delete(Produto t)
         {
             throw new NotImplementedException();
@@ -21,12 +30,63 @@ namespace projeto_sgp_WPFversion.Models
 
         public void Insert(Produto t)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var query = con.Query();
+                query.CommandText = @"
+                    INSERT INTO produtos (nome, preco_compra, preco_venda, quantidade, fornecedor_id)
+                    VALUES (@nome, @preco_compra, @preco_venda, @quantidade, @fornecedor_id)
+                ";
+
+                query.Parameters.AddWithValue("@nome", t.Nome);
+
+
+                var result = query.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                con.Close();
+            }
         }
 
         public List<Produto> List()
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<Produto> produtos = new List<Produto>();
+
+                var query = con.Query();
+                query.CommandText = "SELECT * FROM produtos";
+
+                MySqlDataReader reader = query.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    produtos.Add(new Produto
+                    {
+                        Id = reader.GetUInt32("id"),
+                        Nome = reader.GetString("nome"),
+                        PrecoCompra = reader.GetFloat("preco_compra"),
+                        PrecoVenda = reader.GetFloat("preco_venda"),
+                        Quantidade = reader.GetUInt32("quantidade"),
+                        FornecedorId = reader.GetUInt32("fornecedor_id")
+                    });
+                }
+
+                return produtos;
+            } 
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                con.Close();
+            }
         }
 
         public void Update(Produto t)
